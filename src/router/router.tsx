1ffd2a184421416dcef1db6957de7850c5ad1a34
ACAZUMAH <calebazumah9@gmail.com>
2025-09-28 14:53:53 +0000

    Updates

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { routes } from "./routes";
+import { useEffect } from "react";
+import { useForceUpdate } from "@mantine/hooks";
+import { useAppAuthentication } from "../hooks/useAppAuthentication";
 
 export const AppRouter = () => {
-    
+  const { isAuthenticated } = useAppAuthentication();
+  const forceUpdate = useForceUpdate();
   const getRouter = () => {
     return createBrowserRouter([
       {
@@ -12,5 +16,9 @@ export const AppRouter = () => {
     ]);
   };
 
-  return <RouterProvider router={getRouter()}/>
+  useEffect(() => {
+    forceUpdate();
+  }, [isAuthenticated]);
+
+  return <RouterProvider router={getRouter()} />;
 };
